9f978b02671af7eba1f44254a7c5da87ab194af3
robeh0018 <123769181+robeh0018@users.noreply.github.com>
2023-02-05 06:14:44 +0100

    First commit

---
+++ useTodo.js
@@ -0,0 +1,56 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+export const useTodo = () => {
+
+    const init = () => {
+        return JSON.parse( localStorage.getItem('todos')) || [];
+    };
+
+    const [todos, dispatchTodo] = useReducer(todoReducer, [], init);
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos]);
+    
+    // Funciones necesarias para contar los TODOS.
+    const todosCount = () => {
+        return todos.length;
+    };
+    const pendingTodosCount = () => {
+        return todos.filter(todo => !todo.done).length;
+    };
+
+    // Funciones para el crud de los TODOS.
+    const handleNewTodo = (todo) => {
+        const action = {
+            type: `[TODO] Add Todo`,
+            payload: todo,
+        };
+        dispatchTodo ( action );
+    };
+    const handleDeleteTodo = (id) => {
+        const action = {
+            type: `[TODO] Remove Todo`,
+            payload: id,
+        };
+        dispatchTodo(action);
+    };
+    const handleToggleTodo = (id) => {
+        const action = {
+            type: `[TODO] Toggle Todo`,
+            payload: id,
+        };
+        dispatchTodo(action);
+    };
+
+    return {
+        todos,
+        todosCount,
+        pendingTodosCount,
+        handleDeleteTodo,
+        handleNewTodo,
+        handleToggleTodo,
+    };
+};
+
